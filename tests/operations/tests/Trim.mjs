aef25f3903d432f1d5c7bd6155fee8f1bdc48c16
Gaijin.at <71113078+gaijinat@users.noreply.github.com>
2023-05-10 15:14:45 +0200

    Add files via upload

---
+++ tests/operations/tests/Trim.mjs
@@ -0,0 +1,53 @@
+/**
+ * @author gaijinat [web@gaijin.at]
+ * @copyright Crown Copyright 2018
+ * @license Apache-2.0
+ */
+import TestRegister from "../../lib/TestRegister.mjs";
+
+TestRegister.addTests([
+    {
+        name: "Trim: nothing",
+        input: "",
+        expectedOutput: "",
+        recipeConfig: [
+            {
+                op: "Trim",
+                args: ["Both"]
+            },
+        ],
+    },
+    {
+        name: "Trim: Start",
+        input: "\n\t TEST \t\n",
+        expectedOutput: "TEST \t\n",
+        recipeConfig: [
+            {
+                op: "Trim",
+                args: ["Start"]
+            },
+        ],
+    },
+    {
+        name: "Trim: End",
+        input: "\n\t TEST \t\n",
+        expectedOutput: "\n\t TEST",
+        recipeConfig: [
+            {
+                op: "Trim",
+                args: ["End"]
+            },
+        ],
+    },
+    {
+        name: "Trim: Both",
+        input: "\n\t TEST \t\n",
+        expectedOutput: "TEST",
+        recipeConfig: [
+            {
+                op: "Trim",
+                args: ["Both"]
+            },
+        ],
+    },
+]);
